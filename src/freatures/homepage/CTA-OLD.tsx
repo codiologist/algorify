3be614fe4e3aecff752998de56a0f75b249042f5
codiologist <robin.promoteur@gmail.com>
2025-09-18 16:13:45 +0600

    CTA Secton Done.

--- src/freatures/homepage/CTA-OLD.tsx
+++ src/freatures/homepage/CTA-OLD.tsx
@@ -2,51 +2,33 @@ import { CustomButton } from "@/components/CustomButton";
 import { Card, CardContent } from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
-import blue_shape_mobile from "/public/cta-blue-shape-mobile.png";
-import blue_shape from "/public/cta-blue-shape.png";
-import curve_shape_mobile from "/public/cta-curve-shape-mobile.png";
-import curve_shape_desktop from "/public/cta-curve-shape.png";
 import text_animation from "/public/text-animation-2.gif";
 
 const CTA = () => {
   return (
     <section className="container-xl relative overflow-hidden rounded-xl px-1 py-0 lg:py-10">
-      <Card className="relative gap-0 overflow-hidden rounded-2xl bg-white p-0 pb-20 shadow-none lg:min-h-[450px] lg:border-0">
+      <div
+        className={[
+          "relative overflow-hidden rounded-xl",
+          "before:absolute before:top-1 before:left-1 before:content-['']",
+          "before:h-[750px] before:w-[430px] lg:before:h-[650px] lg:before:w-[660px]",
+          "before:bg-[url('/cta-curve-shape-mobile.png')] before:bg-contain before:bg-no-repeat lg:before:bg-[url('/cta-curve-shape.png')]",
+          "before:pointer-events-none before:z-10 before:opacity-100",
+          "after:absolute after:top-1 after:right-1 after:content-['']",
+          "after:h-[480px] after:w-[410px] lg:after:w-[380px] xl:after:h-[360px]",
+          "after:bg-fill after:bg-[url('/cta-blue-shape-mobile2.png')] after:bg-contain after:bg-no-repeat lg:after:bg-[url('/cta-blue-shape.png')]",
+          "after:pointer-events-none",
+        ].join(" ")}
+      >
         <p className="absolute top-42 left-5 z-10 text-base font-medium lg:top-4">
           Find the Best <span className="text-[#B5B5B5]">Service</span> at
         </p>
         <p className="absolute right-5 bottom-4 z-10 text-base font-medium text-[#676767]">
           Discover Your Potential
         </p>
-        <div className="absolute top-0 left-0">
-          <Image
-            src={curve_shape_desktop}
-            className="hidden h-auto w-[670px] lg:block"
-            alt="Shape"
-          />
-          <Image
-            src={curve_shape_mobile}
-            className="block h-auto w-[670px] lg:hidden"
-            alt="Shape"
-          />
-        </div>
-        <div className="absolute top-0 right-0">
-          <Image
-            src={blue_shape}
-            className="hidden h-auto w-[350px] lg:block"
-            alt="Shape"
-          />
-        </div>
-        <div className="block lg:hidden">
-          <Image
-            src={blue_shape_mobile}
-            className="ml-auto h-auto w-[200px]"
-            alt="Shape"
-          />
-        </div>
-        <CardContent className="relative z-20 pl-5">
-          <div className="lg:pt-14 lg:pl-6">
-            <h2 className="flex items-center gap-x-2 text-5xl leading-tight font-semibold lg:mt-6">
+        <Card className="relative m-1 flex min-h-[450px] flex-col justify-center border-1 border-gray-200 bg-white lg:border-0">
+          <CardContent className="relative z-20 space-y-2 pt-44 pb-10 pl-4 lg:pt-0 lg:pb-0 lg:pl-10">
+            <h2 className="mt-6 flex items-center gap-x-2 text-5xl leading-tight font-semibold">
               Be{" "}
               <Image
                 src={text_animation}
@@ -75,14 +57,14 @@ const CTA = () => {
                 className="ml-0 inline lg:ml-3"
               />
             </h2>
-            <div className="mt-10 text-center lg:text-left">
-              <Link href="#" className="inline-block">
+            <div className="mt-10">
+              <Link href="#" className="mt-6">
                 <CustomButton variant="green">Start Now</CustomButton>
               </Link>
             </div>
-          </div>
-        </CardContent>
-      </Card>
+          </CardContent>
+        </Card>
+      </div>
     </section>
   );
 };
